b5f16cfc52296c169a1551b8ba409b422cbb2b74
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-06-17 13:49:35 -0300

    added tests

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,6 +9,7 @@ module.exports = {
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
+  "ignorePatterns" : ["vite.config.ts", ".eslintrc.cjs"],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
