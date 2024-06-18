f0e81c6fc5169b0c5cec1be85f9f272341406bd3
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-06-18 09:38:22 -0300

    updated readme

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,7 +3,7 @@ import { describe, it, expect } from "vitest";
 import App from "./App";
 
 describe("App", () => {
-  it("should render", () => {
+  it("renders headline", () => {
     render(<App />);
 
     expect(screen.getByRole("heading")).toHaveTextContent("Vite + React");
