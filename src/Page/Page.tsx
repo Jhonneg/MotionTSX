cba50e7a969a5683dbd2d2bfa6ab7af13520d60b
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-08-15 17:19:50 -0300

    started drag n drop implementation

--- src/Page/Page.tsx
+++ src/Page/Page.tsx
@@ -5,28 +5,45 @@ import { Title } from "./Title";
 import { nanoid } from "nanoid";
 import { useAppState } from "../state/AppStateContext";
 import NodeTypeSwitcher from "../Node/NodeTypeSwitcher";
+import { DndContext, DragOverlay, DragOverEvent } from "@dnd-kit/core";
+import {
+  SortableContext,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
+import NodeContainer from "../Node/NodeContainer";
 
 export default function Page() {
-  const { title, nodes, addNode, setTitle } = useAppState();
+  const { title, nodes, addNode, reorderNodes, setTitle } = useAppState();
 
   const [focusedNodeIndex, setFocusedNodeIndex] = useFocusedNodeIndex({
     nodes,
   });
+
+  function handleDragEvent(event: DragOverEvent) {
+    const { active, over } = event;
+    if (over?.id && active.id !== over?.id) {
+      reorderNodes(active.id as string, over.id as string);
+    }
+  }
 
   return (
     <>
       <Cover />
       <div>
         <Title addNode={addNode} title={title} changePageTitle={setTitle} />
-        {nodes.map((node, index) => (
-          <NodeTypeSwitcher
-            key={node.id}
-            node={node}
-            isFocused={focusedNodeIndex === index}
-            updateFocusedIndex={setFocusedNodeIndex}
-            index={index}
-          />
-        ))}
+        <DndContext onDragEnd={handleDragEvent}>
+          <SortableContext items={nodes} strategy={verticalListSortingStrategy}>
+            {nodes.map((node, index) => (
+              <NodeContainer
+                key={node.id}
+                node={node}
+                isFocused={focusedNodeIndex === index}
+                updateFocusedIndex={setFocusedNodeIndex}
+                index={index}
+              />
+            ))}
+          </SortableContext>
+        </DndContext>
         <Spacer
           showHint={!nodes.length}
           handleClick={() => {
